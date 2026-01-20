351490158069ff825b47bc86acbd3687d391e6a3
SchmuckeDj <schjosepu@gmail.com>
2026-01-20 18:49:26 -0400

    week2 temples

---
+++ scripts/temples.js
@@ -0,0 +1,14 @@
+const year = document.querySelector("#year");
+const lastModified = document.querySelector("#lastModified");
+
+year.textContent = new Date().getFullYear();
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
+
+
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {
+  navigation.classList.toggle("open");
+  menuButton.classList.toggle("open");
+});
